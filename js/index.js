fb78695a23dce506aeadaa57108d9fe3384c5178
suga <2326887587@qq.com>
2022-11-01 21:32:49 +0800

    大致完成版

---
+++ js/index.js
@@ -0,0 +1,32 @@
+let localNav = document.querySelector('.local-nav');
+list.forEach(item => {
+    let listLi = document.createElement('li');
+    let listImg = document.createElement('img');
+    localNav.appendChild(listLi);
+    listImg.src = item;
+    listLi.appendChild(listImg);
+})
+
+let commodity = document.querySelector('.commodity');
+books.forEach(item => {
+    let commodityLi = document.createElement('li');
+    let commodityImg = document.createElement('img');
+    let commodityMessage = document.createElement('div');
+    commodityMessage.className = 'message';
+    let commodityTitle = document.createElement('h2');
+    let commodityContent = document.createElement('p');
+    let commodityPrice = document.createElement('h3');
+    let commodityOld = document.createElement('span');
+    commodityImg.src = item.thumbSrc;
+    commodityTitle.innerHTML = item.bookName;
+    commodityContent.innerHTML = item.content;
+    commodityPrice.innerHTML = "￥" + item.presentPrice;
+    commodityOld.innerHTML = "￥" + item.originalPrice;
+    commodityLi.appendChild(commodityImg);
+    commodityMessage.appendChild(commodityTitle);
+    commodityMessage.appendChild(commodityContent);
+    commodityMessage.appendChild(commodityPrice);
+    commodityPrice.appendChild(commodityOld);
+    commodityLi.appendChild(commodityMessage)
+    commodity.appendChild(commodityLi);
+})
